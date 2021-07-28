9eff6ca09f1c5bb239946f13f67299f0a7588dc5
Linda Xiao <lindax28@hotmail.com>
2021-07-27 20:40:00 -0700

    Style signin/signup pages

--- client/src/components/session/login.tsx
+++ client/src/components/session/login.tsx
@@ -1,8 +1,41 @@
 import { useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import config from '../../config/keys';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Logo from '../../images/moneypot_logo.jpg';
+import Moneypot from '../../images/moneypot_name.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Login() {
+  const classes = useStyles();
   const [email, setEmail] = useState<string>("")
   const [error, setError] = useState<string>("")
   const [password, setPassword] = useState<string>("")
@@ -19,14 +52,68 @@ export default function Login() {
       setError("User not found")
     })
   }
-
+  
   return (
-    <div>
-      <h1>Login</h1>
-      <h4>{error}</h4>
-      <input type="text" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-      <button onClick={login}>Login</button>
-    </div>
-  )
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <div>
+          <Link href="/"><img className="moneypot-logo" src={Logo} alt="Moneypot Logo"/></Link>
+          <Link href="/"><img className="moneypot-name" src={Moneypot} alt="Moneypot Name"/></Link>
+        </div><br></br>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <h3>{error}</h3>
+        <form className={classes.form} noValidate>
+          <TextField
+            onChange={e => setEmail(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            onChange={e => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            onClick={login}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+            </Grid>
+            <Grid item><br></br>
+              <Link href="/register" variant="body1">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 }
--- client/src/components/session/signup.tsx
+++ client/src/components/session/signup.tsx
@@ -1,12 +1,44 @@
 import { useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import config from '../../config/keys';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Logo from '../../images/moneypot_logo.jpg';
+import Moneypot from '../../images/moneypot_name.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Signup() {
   const [name, setName] = useState<string>("")
   const [error, setError] = useState<string>("")
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
+  const classes = useStyles();
 
   const register = () => {
     axios.post(`${config.API_URL}/register`, {
@@ -22,15 +54,77 @@ export default function Signup() {
     })
   }
 
-
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <h4>{error}</h4>
-      <input type="text" placeholder="Full Name" onChange={e => setName(e.target.value)} />
-      <input type="text" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-      <button onClick={register}>Register</button>
-    </div>
-  )
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <div>
+          <Link href="/"><img className="moneypot-logo" src={Logo} alt="Moneypot Logo"/></Link>
+          <Link href="/"><img className="moneypot-name" src={Moneypot} alt="Moneypot Name"/></Link>
+        </div><br></br>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <h3>{error}</h3>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                onChange={e => setName(e.target.value)}
+                autoComplete="fname"
+                name="fullName"
+                variant="outlined"
+                required
+                fullWidth
+                id="fullName"
+                label="Full Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={e => setEmail(e.target.value)}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={e => setPassword(e.target.value)}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            onClick={register}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 }
